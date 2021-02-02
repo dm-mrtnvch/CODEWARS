fc4222035846f0d255a6ffe31ed03c9e82d04037
DM-MRTNVCH <dmvshn@gmail.com>
2021-02-02 23:26:16 +0300

    update

--- 8_kyu.js
+++ 8_kyu.js
@@ -1,14 +1,14 @@
-// Invert values
+// 1. Invert values
 function invert(array) {
     return array.map(number => number * -1)
 }
 
-// Beginner - Lost Without a Map
+// 2. Beginner - Lost Without a Map
 function maps(x){
     return x.map(number => number * 2)
 }
 
-// Count of positives / sum of negatives
+// 3. Count of positives / sum of negatives
 function countPositivesSumNegatives(input) {
     if(input == null || input.length == 0) return []
     let positive = 0
@@ -17,75 +17,75 @@ function countPositivesSumNegatives(input) {
     return [positive, negative]
 }
 
-// A Needle in the Haystack
+// 4. A Needle in the Haystack
 function findNeedle(haystack) {
     return 'found the needle at position ' + haystack.findIndex(ndl => ndl === 'needle')
 }
 
-// Abbreviate a Two Word Name
+// 5. Abbreviate a Two Word Name
 function abbrevName(name){
     return name.split(' ').map(n => n[0].toUpperCase()).join('.')
 }
 
-// Convert number to reversed array of digits
+// 6. Convert number to reversed array of digits
 function digitize(n) {
     return n.toString().split('').map(num => parseInt(num)).reverse()
 }
 
-// Keep Hydrated!
+// 7. Keep Hydrated!
 function litres(time) {
     return Math.floor(time / 2)
 }
 
-// Basic Mathematical Operations
+// 8. Basic Mathematical Operations
 function basicOp(operation, value1, value2) {
     return eval(value1 + operation + value2)
 }
 
-// Century From Year
+// 9. Century From Year
 function century(year) {
     return Math.ceil(year / 100)
 }
 
-// Is n divisible by x and y?
+// 10. Is n divisible by x and y?
 const isDivisible = (n, x, y) => n % x == 0 && n % y == 0
 
-// Square(n) Sum
+// 11. Square(n) Sum
 function squareSum(numbers){
     return numbers.reduce((result, number) => result + (Math.pow(number, 2)), 0) // (number * number)
 }
 
-// Convert a Number to a String!
+// 12. Convert a Number to a String!
 function numberToString(num) {
     return `${num}`
 }
 
-// Counting sheep...
+// 13. Counting sheep...
 function countSheeps(arrayOfSheep) {
     return arrayOfSheep.filter(sheep => sheep === true).length
 }
 
-// Convert boolean values to strings 'Yes' or 'No'.
+// 14. Convert boolean values to strings 'Yes' or 'No'.
 function boolToWord( bool ){
     return bool ? 'Yes' : 'No'
 }
 
-// Reversed Strings
+// 15. Reversed Strings
 function solution(str){
     return str.split('').reverse().join('')
 }
 
-// Grasshopper - Summation
+// 16. Grasshopper - Summation
 var summation = function (num) {
     return result = (num * (num + 1)) / 2;
 }
 
-// Remove String Spaces
+// 17. Remove String Spaces
 function noSpace(x){
     return x.split(' ').join('')
 }
 
-// String repeat
+// 18. String repeat
 function repeatStr (n, s) {
     if (n > 0)
         return s.repeat(n);
@@ -93,30 +93,83 @@ function repeatStr (n, s) {
         return "";
 }
 
-// Return Negative
+// 19. Return Negative
 function makeNegative(num) {
     return num > 0 ? -num : num
 }
 
-// Remove First and Last Character
+// 20. Remove First and Last Character
 const removeChar = str => str.substr(1, str.length -2);
 
-// Opposite number
+// 21. Opposite number
 function opposite(number) {
     return -1 * number
 }
 
-// Sum of positive
+// 22. Sum of positive
 function positiveSum(arr) {
     return !arr.length ? 0 : arr.reduce((result, number) => number > 0 ? result + number : result, 0);
 }
 
-// Even or Odd
+// 23. Even or Odd
 function even_or_odd(number) {
     return number % 2 ? 'Odd' : 'Even'
 }
 
-// Multiply
+// 24. Multiply
 function multiply(a, b){
     return a * b
+}
+
+// 25. Beginner Series #2 Clock
+function past(h, m, s){
+    return h * 3600000 + m * 60000 + s * 1000
+}
+
+// 26. Total amount of points
+function points(games) {
+    let point
+    let result = games.map(num => {
+        if(Number(num.charAt(0)) > Number(num.charAt(2))) {
+            return point = 3
+        } else if(Number(num.charAt(0)) === Number(num.charAt(2))){
+            return point = 1
+        } else {
+            return point = 0
+        }
+    })
+
+    return result.reduce((x, y) => x + y)
+}
+
+// 27. Convert a String to a Number!
+let stringToNumber = function(str){
+    return +str;
+}
+
+// 28. Sum without highest and lowest number
+function sumArray(array) {
+    if(!array) {
+        return 0
+    }
+    let sum = 0
+    array.sort((a, b) => a - b).forEach((a, i) => {
+        if(i !== array.length - 1 && i !== 0) {
+            sum += a
+        }
+    })
+    return sum
+}
+
+// 29. Fake Binary
+function fakeBin(x){
+    return x.split('').map(function(num) {
+        return num < 5 ? '0' : '1'
+    }).join('')
+}
+
+// 30. Function 1 - hello world
+function greet() {
+    let message = 'hello world!'
+    return message
 }
