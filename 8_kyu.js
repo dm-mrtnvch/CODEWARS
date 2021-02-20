ece028a58eb9ad58db357174998ecc1c64c40249
DM-MRTNVCH <dmvshn@gmail.com>
2021-02-20 12:15:17 +0300

    08.271-280

--- 8_kyu.js
+++ 8_kyu.js
@@ -1960,3 +1960,116 @@ websites.push('codewars')
 function iceBrickVolume(radius, bottleLength, rimLength) {
     return ((bottleLength-rimLength)*(radius*2)*radius);
 }
+
+// 271. Shifty Closures
+let name;
+
+let greet_abe = function() {
+    name = 'Abe';
+    return "Hello, " + name + '!';
+};
+
+let greet_ben = function() {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+};
+
+// 272. Circular Objects #1 - Running around in circles
+const circular = {
+    value: "Hello World"
+};
+circular.self = circular;
+
+// 273. Learning TypeScript. Basic Types. Type Assertions
+export class SuccessServerResult {
+    constructor (public httpCode: number, public resultObject:Object) {}
+}
+
+export class ErrorServerResult {
+    constructor (public httpCode: number, public message:string) {}
+}
+
+export function getResult(result: SuccessServerResult | ErrorServerResult) {
+    if (result.httpCode === 200) {
+        return (result as SuccessServerResult).resultObject;
+    } else {
+        return (result as ErrorServerResult).message;
+    }
+}
+
+// 274. noobCode 02: TRICKY QUESTIONS ( primitives and operator precedence)
+function greaterThanLessThan (a,b,c) {
+    return a<b<c
+}
+
+// 275. Lexical this
+let Person = function() {
+    let person = {
+        _name: "Leroy",
+        _friends: [],
+        fillFriends(f) {
+            this._friends.push(...f)
+        }
+    }
+    return person
+}
+
+// 276. Semi-Optional
+function wrap(value) {
+    return {value};
+}
+
+// 277. Hello Happy Codevarrior!
+class Warrior {
+    constructor(newName) {
+        this.Name = newName
+    }
+    name(newName) {
+        if (newName) {
+            this.Name = newName
+        }
+        return this.Name
+    }
+}
+
+Warrior.prototype.toString = function() {
+    return `Hi! my name's ${this.Name}`
+}
+
+// 278. Learning TypeScript. Basic Types
+export enum Color {Red = 1, Green = 2, Blue = 4}
+export const var1Boolean: boolean = true;
+export const var2Decimal: number = 13;
+export const var3Hex : number = 0xf00d;
+export const var4Binary : number = 0b111111;
+export const var5Octal : number = 0o744;
+export const var6String : string = 'Hello, world!';
+export const var7Array : any[] = [1, 'test', {a: 3}, 4, 5];
+export const var8NumericArray : number[] = [1, 2, 3, 4, 5];
+export const var9Tuple  : [string,number] = ['key', 12345];
+export const var10Enum   : Color = Color.Blue;
+export const var11ArrayOfAny   : Array<any> = [1, 'test', {a: 3}, 4, 5];
+export const var12VoidFunction =(): void =>{}
+export const var13Null   : null = null;
+export const var14Undefined   : undefined = undefined;
+export const var15NeverFunction =():never=>{throw new Error('')};
+
+// 279. Max Headroom and JavaScript style
+function getMax1() {
+    let max = {
+        name: 'Max Headroom',
+    }
+    return max;
+}
+
+function getMax2() {
+    return {
+        name: 'Max Headroom',
+    }
+}
+
+// 280. Return to Sanity
+function mystery() {
+    let results = {sanity: 'Hello'};
+    return results;
+}
